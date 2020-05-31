d8f96e3048915a5d23bd9452be325ef8987d303c
Casey Milne <joel@goldhat.ca>
2020-05-30 21:49:11 -0500

    question single template

--- components/exam/templates/singles/question.php
+++ components/exam/templates/singles/question.php
@@ -1,19 +1,17 @@
 <?php get_header(); ?>
 
-<div class="question-single">
-
-  <header>
-    Question
-  </header>
-
+<div class="question question-<?php print $question->id; ?>" data-question-id="<?php print $question->id; ?>">
+  <h3>Question X</h3>
+  <h1><?php print $question->title; ?></h1>
+  <h4>Select your answer</h4>
+  <ul class="selectable">
+    <?php foreach( $question->options as $option ) : ?>
+      <li class="question-option question-option-<?php print $option->id; ?>"
+        data-question-id="<?php print $question->id; ?>"
+        data-question-option-id="<?php print $option->id; ?>">
+          <?php print $option->label; ?></li>
+    <?php endforeach; ?>
+  </ul>
 </div>
-
-<?php
-
-print '<pre>';
-var_dump( $question );
-print '</pre>';
-
-?>
 
 <?php get_footer(); ?>
