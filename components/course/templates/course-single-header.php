4f14b1b9d9fbc1486046467f266ca5a4db68aac7
Casey Milne <joel@goldhat.ca>
2020-06-06 18:49:05 -0500

    course single header title intro

--- components/course/templates/course-single-header.php
+++ components/course/templates/course-single-header.php
@@ -8,6 +8,8 @@ $access->courseAccess( $course );
 ?>
 
 <header>
+  <h1><?php print $course->title; ?></h1>
+  <div><?php print $course->intro; ?></div>
   <?php
     if( !$access->grant ):
       $access->renderBlockMessage();
