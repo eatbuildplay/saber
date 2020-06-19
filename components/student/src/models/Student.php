382f01da6fc05a3994a0809c91f6b585025f6917
Casey Milne <joel@goldhat.ca>
2020-06-19 03:06:32 -0500

    get membership data

--- components/student/src/models/Student.php
+++ components/student/src/models/Student.php
@@ -2,9 +2,12 @@
 
 namespace Saber\Student\Model;
 
+define('SABER_PREMIUM_MEMBERSHIP_PRODUCT', 5111);
+
 class Student {
 
   public $user;
+  public $membership;
 
   public function __construct() {
 
@@ -12,9 +15,42 @@ class Student {
 
   }
 
+  public function getMembership() {
+    return $this->membership;
+  }
+
+  public function hasPremiumMembership() {
+
+    return false;
+
+  }
+
+  public function loadMembership() {
+
+    $check = \WC_Subscriptions_Manager::user_has_subscription(
+      $this->user->ID,
+      SABER_PREMIUM_MEMBERSHIP_PRODUCT,
+      'active'
+    );
+
+    if( $check ) {
+      $membership = 'premium';
+    } else {
+      $membership = 'free';
+    }
+
+    return $membership;
+
+  }
+
+  public function loadById( $studentId ) {
+
+  }
+
   public static function load() {
     $student = new Student;
     $student->user = wp_get_current_user();
+    $student->membership = $student->loadMembership();
     return $student;
   }
 
