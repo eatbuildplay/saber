2b73168425965b41be9a69fe88e2dafbb96167a3
Casey Milne <joel@goldhat.ca>
2020-06-21 11:36:28 -0500

    dashboard widget styles

--- components/dashboard/templates/widgets.php
+++ components/dashboard/templates/widgets.php
@@ -2,22 +2,45 @@
 
   <h2>Registered Courses</h2>
   <div class="registered-courses-list">
-    <?php
-      if( !empty( $courses )) :
-        foreach( $courses as $cr ):
-    ?>
-      <h3><?php print $cr->course->title; ?></h3>
-      <a href="<?php print $cr->course->permalink ?>">Study</a>
-    <?php endforeach; endif; ?>
 
-    <?php if( empty( $courses )) : ?>
-      <p>Register for your first <a href="<?php print site_url('courses'); ?>">course</a>.</p>
-    <?php endif; ?>
+    <table>
+      <thead>
+        <tr>
+          <th>Course</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+
+      <tbody>
+
+        <?php
+          if( !empty( $courses )) :
+            foreach( $courses as $cr ):
+        ?>
+          <tr>
+            <td><?php print $cr->course->title; ?></td>
+            <td><a href="<?php print $cr->course->permalink ?>">Study</a></td>
+          </tr>
+
+          <?php endforeach; endif; ?>
+
+          <?php if( empty( $courses )) : ?>
+            <tr>
+              <td colspan="2">
+                <p>Register for your first <a href="<?php print site_url('courses'); ?>">course</a>.</p>
+              </td>
+            </tr>
+          <?php endif; ?>
+
+      </tbody>
+    </table>
+
+
 
   </div>
 
   <div class="view-all-courses">
-    <a href="<?php print site_url('courses'); ?>">View All Courses</a>
+    <a class="block-link" href="<?php print site_url('courses'); ?>">View All Courses</a>
   </div>
 
 </div>
@@ -29,13 +52,49 @@
 
     <?php if( $student->membership == 'free' ): ?>
       <h4>You are a free member.</h4>
-      <a href="https://spanish10.com/upgrade/">Upgrade to Premium Membership</button>
+      <a class="block-link" href="https://spanish10.com/upgrade/">Upgrade to Premium Membership</a>
     <?php endif; ?>
 
     <?php if( $student->membership == 'premium' ): ?>
       <h4>You are a premium member.</h4>
-      <a href="https://spanish10.com/account">Review your account</button>
+      <a href="https://spanish10.com/account">Review your account</a>
     <?php endif; ?>
+
+  </div>
+
+</div>
+
+<!-- Activity Log -->
+<div class="dashboard-widget dashboard-widget-activity">
+
+  <h2>Activity Log</h2>
+  <div class="activity-log">
+
+    <table>
+      <thead>
+        <tr>
+          <th></th>
+        </tr>
+      </thead>
+    </table>
+
+  </div>
+
+</div>
+
+<!-- Achievements -->
+<div class="dashboard-widget dashboard-widget-achievement">
+
+  <h2>Student Achievements</h2>
+  <div class="achievements-list">
+
+    <table>
+      <thead>
+        <tr>
+          <th></th>
+        </tr>
+      </thead>
+    </table>
 
   </div>
 
