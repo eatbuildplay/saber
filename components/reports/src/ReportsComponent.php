fb1bfacbfb2afa3639d1ce0e855a9d57b42f1d1e
Casey Milne <joel@goldhat.ca>
2020-08-03 09:25:30 -0500

    draft charts for reports

--- components/reports/src/ReportsComponent.php
+++ components/reports/src/ReportsComponent.php
@@ -6,39 +6,27 @@ class ReportsComponent {
 
   public function __construct() {
 
-
+    add_action('admin_enqueue_scripts', [$this, 'adminScripts']);
 
   }
 
   public function pageCallback() {
+
+    $template = new \Saber\Template;
+    $template->path = 'components/reports/templates/';
+    $content = '';
 
     $userCount = count_users();
 
     // $cts = Content Counts []
     $cts = self::fetchContentCounts();
 
-    $content = '';
-
-    $content .= '<h2>' + $cts->course + '</h2>';
-    $content .= '<h4>Course Count</h4>';
-
-    $content .= '<h2>' + $cts->courseRegistration + '</h2>';
-    $content .= '<h4>Course Registrations</h4>';
-
-    $content .= '<h2>' + $cts->lesson + '</h2>';
-    $content .= '<h4>Lesson Count</h4>';
-
-    $content .= '<h2>' + $cts->exam + '</h2>';
-    $content .= '<h4>Exam Count</h4>';
-
-    $content .= '<h2>' + $cts->exam + '</h2>';
-    $content .= '<h4>Exam Attempts</h4>';
-
-    $content .= '<h2>' + $cts->question + '</h2>';
-    $content .= '<h4>Question Count</h4>';
-
-    $content .= '<h2>' + $userCount['total_users'] + '</h2>';
-    $content .= '<h4>Student Count</h4>';
+    $template->name = 'chart';
+    $template->data = [
+      'userCount' => $userCount,
+      'cts'       => $cts
+    ];
+    $content .= $template->get();
 
     print $content;
 
@@ -49,13 +37,32 @@ class ReportsComponent {
     $cts = new \stdClass;
 
     $cts->course = \wp_count_posts('course')->publish;
+    $cts->courseRegistration = \wp_count_posts('course_registration')->publish;
     $cts->lesson = \wp_count_posts('lesson')->publish;
     $cts->exam = \wp_count_posts('exam')->publish;
     $cts->examScore = \wp_count_posts('exam_score')->publish;
     $cts->question = \wp_count_posts('question')->publish;
-    $cts->courseRegistration = \wp_count_posts('course_registration')->publish;
 
     return $cts;
+
+  }
+
+  public function adminScripts() {
+
+    wp_enqueue_style(
+      'saber-reports',
+      SABER_URL . 'components/reports/assets/reports.css',
+      array(),
+      true
+    );
+
+    wp_enqueue_script(
+      'saber-reports',
+      SABER_URL . 'components/reports/assets/reports.js',
+      array('jquery', 'chartjs'),
+      SABER_VERSION,
+      true
+    );
 
   }
 
